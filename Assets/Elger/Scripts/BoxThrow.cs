b99ad1c93e961edf4cf0ad834a856ea2f2ce8896
ElgerC <elger.creutzberg@gmail.com>
2024-09-25 13:10:22 +0200

    added DOOS object

--- Assets/Elger/Scripts/BoxThrow.cs
+++ Assets/Elger/Scripts/BoxThrow.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum BoxStates
+{
+    InHands,
+    Launched,
+    Frozen
+}
+
 public class BoxThrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    BoxStates m_boxStates;
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            switch (m_boxStates)
+            {
+                case BoxStates.InHands:
+                    break;
+                case BoxStates.Launched:
+                    break;
+                case BoxStates.Frozen:
+                    break;
+            }
+        }
     }
 }
