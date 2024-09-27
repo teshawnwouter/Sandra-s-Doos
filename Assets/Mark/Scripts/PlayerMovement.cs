5b85141e2617a9170eb7866007e920edfd62d742
Akash <akashlochan2005@gmail.com>
2024-09-27 09:23:10 +0200

    Image fixed yay

--- Assets/Mark/Scripts/PlayerMovement.cs
+++ Assets/Mark/Scripts/PlayerMovement.cs
@@ -19,8 +19,8 @@ public class PlayerMovement : MonoBehaviour
     private float jumpTimer = 0;
     private float jumpTrigger = 0.3f;
 
-    public Image jumpscareImage;
-    public float flashDuration = 1f;
+    public GameObject jumpscareImage;
+    public float flashDuration = 0.2f;
     private bool jumpscareTriggered = false;
     public int coins = 0;
 
@@ -28,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_rigidbody = GetComponent<Rigidbody>();
         jumpForcer = GameObject.Find("JumpForcing");
-        jumpscareImage = FindObjectOfType<Image>();
+        //jumpscareImage = FindObjectOfType<Image>();
     }
 
     void Update()
@@ -92,10 +92,10 @@ public class PlayerMovement : MonoBehaviour
     private IEnumerator FlashJumpscare()
     {
         jumpscareTriggered = true;
-        jumpscareImage.enabled = true;
+        jumpscareImage.SetActive(true);
         Debug.Log(jumpscareImage);
         yield return new WaitForSeconds(flashDuration);
         Debug.Log("test");
-        jumpscareImage.enabled = false;
+        jumpscareImage.SetActive(false);
     }
 }
